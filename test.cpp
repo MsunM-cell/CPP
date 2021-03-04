0d0ead6ab1ed5f0544a5b9c02ee85b20dad6cfeb
MsunM-cell <2464831035@qq.com>
2021-03-04 18:00:59 +0800

    test

--- test.cpp
+++ test.cpp
@@ -2,30 +2,9 @@
 
 using namespace std;
 
-class A
-{
-public:
-    A(int n)
-    {
-        x = n;
-    }
-    int get()
-    {
-        return x;
-    }
-
-private:
-    int x;
-};
-
 int main(void)
 {
-    A a[3] = {
-        A(1),
-        A(4),
-        A(5)};
-    for (int i = 0; i < 3; i++)
-        cout << a[i].get() << endl;
+    cout << "hello world" << endl;
 
     return 0;
 }
